a1946a88b5fb240efc67f6462a17398c1696cb3f
Raul Rotundo <raulrotundo@gmail.com>
2018-01-03 19:34:04 -0300

    Add new home module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeContainer from 'modules/home/home-container';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render((
+  <Router>
+    <Switch>
+      <Route exact path="/" component={HomeContainer}></Route>
+    </Switch>
+  </Router>
+), document.getElementById('root'))
