eea561aed8040781d88ad23cc1d562426365188b
Raul Rotundo <raulrotundo@gmail.com>
2018-01-04 23:01:49 -0300

    Initial FooterComponent

---
+++ src/app/modules/layout/footer/footer-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import FooterComponent from './footer-component';
+
+class FooterContainer extends Component {
+
+  render() {
+    return <FooterComponent />;
+  }
+}
+
+export default FooterContainer;
