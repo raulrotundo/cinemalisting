1168a495bbec09a330ac8e1665d14656203b5eba
Raul Rotundo <raulrotundo@gmail.com>
2018-01-04 23:03:06 -0300

    Refactoring initial index layout structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomeContainer from 'modules/home/home-container';
+import 'semantic-ui-css/semantic.min.css';
+import LayoutComponent from 'modules/layout/layout-component';
 
 ReactDOM.render((
   <Router>
     <Switch>
-      <Route exact path="/" component={HomeContainer}></Route>
+      <Route exact path="/" component={LayoutComponent}></Route>
     </Switch>
   </Router>
 ), document.getElementById('root'))
