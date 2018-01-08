9e35c5aced13e49ececfa8a1d829d32d9c99687c
Raul Rotundo <raulrotundo@gmail.com>
2018-01-08 20:38:55 -0300

    Getting slider settings by redux init configuration

--- src/app/modules/layout/content/home/slider/slider-container.js
+++ src/app/modules/layout/content/home/slider/slider-container.js
@@ -1,50 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import SliderComponent from './slider-component';
 
 class SliderContainer extends Component {
 
-  constructor(props) {
-    super(props);
-    this.settings = {
-      dots: false,
-      arrows: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            infinite: false
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
-  }
-
   render() {
     return <SliderComponent
-      settings={this.settings}
+      settings={this.props.settings}
       title={this.props.title}
     />;
   }
 }
 
-export default SliderContainer;
+SliderContainer.propTypes = {
+  title: PropTypes.string.isRequired,
+  settings: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  return state.slider;
+}
+
+const mapDispatchToProps = dispatch => {
+  return {}
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SliderContainer);
