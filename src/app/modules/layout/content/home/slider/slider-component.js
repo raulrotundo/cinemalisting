e28c50eebc91b67ec774bfe4dab50808565eb03e
Raul Rotundo <raulrotundo@gmail.com>
2018-01-10 11:01:49 -0300

    Move most popular movies to a mock file

--- src/app/modules/layout/content/home/slider/slider-component.js
+++ src/app/modules/layout/content/home/slider/slider-component.js
@@ -1,43 +1,11 @@
 import React from 'react';
 import { Container, Image } from 'semantic-ui-react';
 import Slider from 'react-slick';
+import MostPopularMoviesMock from 'mock/most-popular-movies.json';
 
 const SliderComponent = (props) => {
   const { settings, title } = props;
-  const sliderInfo = [
-    {
-      id: 1,
-      imgSrc: 'http://via.placeholder.com/143x231'
-    },
-    {
-      id: 2,
-      imgSrc: 'http://via.placeholder.com/143x231'
-    },
-    {
-      id: 3,
-      imgSrc: 'http://via.placeholder.com/143x231'
-    },
-    {
-      id: 4,
-      imgSrc: 'http://via.placeholder.com/143x231'
-    },
-    {
-      id: 5,
-      imgSrc: 'http://via.placeholder.com/143x231'
-    },
-    {
-      id: 6,
-      imgSrc: 'http://via.placeholder.com/143x231'
-    },
-    {
-      id: 7,
-      imgSrc: 'http://via.placeholder.com/143x231'
-    },
-    {
-      id: 8,
-      imgSrc: 'http://via.placeholder.com/143x231'
-    }
-  ];
+  const sliderInfo = MostPopularMoviesMock;
   return (
     <Container>
       <h2>{title}</h2>
