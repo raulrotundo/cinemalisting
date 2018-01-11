d6aea8f40a9dc8056ddb7fa8557a545077e5b347
Raul Rotundo <raulrotundo@gmail.com>
2018-01-11 13:27:07 -0300

    Update number of slide to scroll

--- src/app/redux/reducers/slider.js
+++ src/app/redux/reducers/slider.js
@@ -5,15 +5,15 @@ const initialState = {
     dots: false,
     arrows: true,
     infinite: false,
-    speed: 500,
+    speed: 1500,
     slidesToShow: 6,
-    slidesToScroll: 1,
+    slidesToScroll: 6,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 1,
+          slidesToScroll: 4,
           infinite: false
         }
       },
@@ -21,14 +21,14 @@ const initialState = {
         breakpoint: 600,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1
+          slidesToScroll: 3
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
+          slidesToScroll: 2
         }
       }
     ]
