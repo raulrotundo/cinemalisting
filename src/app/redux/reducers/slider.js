c2bbbfa42a733aa6996a8c0c39adcf0703393553
Raul Rotundo <raulrotundo@gmail.com>
2018-01-12 11:37:42 -0300

    Settings at the top of the home page added

--- src/app/redux/reducers/slider.js
+++ src/app/redux/reducers/slider.js
@@ -32,6 +32,36 @@ const initialState = {
         }
       }
     ]
+  },
+  settingsTop: {
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    variableWidth: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   }
 };
 
