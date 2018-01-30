b208e4994486099ee9006fdd62070f2e940479b2
Raul Rotundo <raulrotundo@gmail.com>
2018-01-29 21:41:42 -0300

    Fix test SliderTopComponent

--- src/app/components/layout/content/home/slider-top/slider-top-component.js
+++ src/app/components/layout/content/home/slider-top/slider-top-component.js
@@ -5,17 +5,19 @@ import Slider from 'react-slick';
 const SliderComponent = (props) => {
   const { settings, sliderData } = props;
   return (
-    <Container>
-      <Slider {...settings}>
-        {sliderData.map((slider) =>
-          <div key={slider.id}>
-            <h3>
-              <Image src={process.env.REACT_APP_POSTER_PATH_W500_H281 + slider.poster_path} alt={slider.title} rounded />
-            </h3>
-          </div>
-        )}
-      </Slider>
-    </Container>
+    <div>
+      <Container>
+        <Slider {...settings}>
+          {sliderData.map((slider) =>
+            <div key={slider.id}>
+              <h3>
+                <Image src={process.env.REACT_APP_POSTER_PATH_W500_H281 + slider.poster_path} alt={slider.title} rounded />
+              </h3>
+            </div>
+          )}
+        </Slider>
+      </Container>
+    </div>
   );
 }
 
