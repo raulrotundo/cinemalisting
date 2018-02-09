fbcfaa9c5fa058899a10e0178895e6253aea34e1
Raul Rotundo <raulrotundo@gmail.com>
2018-02-08 22:37:47 -0300

    Fix HomeComponent test using shallow render

--- src/app/components/layout/content/home/home-component.test.js
+++ src/app/components/layout/content/home/home-component.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import HomeComponent from './home-component';
+import MostPopularMoviesMock from 'mock/most-popular-movies.json';
+import MoviesInTheatersMock from 'mock/in-theaters-movies.json';
+import CommingSoonMoviesMock from 'mock/comming-soon-movies.json';
+import HighestRatedMoviesMock from 'mock/highest-rated-movies.json';
 
 describe('HomeComponent Test', () => {
-  it('render HomeComponent without crashing', () => {
-    shallow(<HomeComponent />);
+  const mostPopularMovies = MostPopularMoviesMock.results;
+  const moviesInTheaters = MoviesInTheatersMock.results;
+  const commingSoonMovies = CommingSoonMoviesMock.results;
+  const highestRatedMovies = HighestRatedMoviesMock.results;
+
+  let props = {
+    mostPopularMovies: mostPopularMovies,
+    moviesInTheaters: moviesInTheaters,
+    commingSoonMovies: commingSoonMovies,
+    highestRatedMovies: highestRatedMovies
+  };
+
+  it('should render HomeComponent without crashing', () => {
+    const wrapper = shallow(<HomeComponent {...props} />);
+    expect(wrapper).toBeDefined();
   });
 });
